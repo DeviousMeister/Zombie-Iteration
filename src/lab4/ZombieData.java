358754bf92fbf3c4d36f9a093f87913457b16ad7
Bill Siever <bsiever@gmail.com>
2019-09-29 13:09:05 -0500

    Initial commit

---
+++ src/lab4/ZombieData.java
@@ -0,0 +1,27 @@
+package lab4;
+
+import edu.princeton.cs.introcs.In;
+import edu.princeton.cs.introcs.StdDraw;
+import support.cse131.ArgsProcessor;
+import java.awt.*;
+
+public class ZombieData {
+    public static void main(String[] args) {
+        ArgsProcessor ap = new ArgsProcessor(args);
+        String fileName = "";
+
+        // TODO: 1. Prompt for the fileName and put it in the fileName variable
+
+        // DONE: This will open the file
+        In inFile = new In(fileName);   // Note that this assumes you're using the fileName variable!
+
+
+        // TODO: 2. Read in the number of entities from the file
+
+        // TODO: 3. Create the arrays that will hold entity data
+
+        // TODO: 4. Read in all the Entity data
+
+        // TODO: 5. Iterate through all the data and display it using StdDraw
+    }
+}
